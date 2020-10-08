7e442eaa470df9bde0aced4d441fd3311ff352cc
Estelle Staffieri <estellelareinne@gmail.com>
2020-10-08 14:33:12 -0600

    Build describe block and first it block for Turn-test

---
+++ src/Turn.js
@@ -0,0 +1,2 @@
+const Card = require("../src/Card")
+
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -5,3 +5,18 @@ const dummyData = require("./dummy-data");
 const Card = require("../src/Card");
 const Turn = require("../src/Turn");
 
+describe("Turn", function() {
+    let card, guess1, guess2, turn1, turn2;
+    beforeEach(function() {
+    card = new Card(dummyData.id, dummyData.question, dummyData.answers, dummyData.correctAnswer);
+    // guess1 =
+    // guess2 =
+    // turn1 = 
+    // turn2 =
+    });
+
+it("should be a function", function() {
+    expect(Turn).to.be.a("function");
+})
+});
+
