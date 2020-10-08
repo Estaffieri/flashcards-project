7cc72bbeadf840b92eab4a6549be9ec6c388267a
Estelle Staffieri <estellelareinne@gmail.com>
2020-10-08 13:27:46 -0600

    Correct ids for dummy dataset and export the file

--- test/dummy-data.js
+++ test/dummy-data.js
@@ -4,23 +4,27 @@ const dummyData = [{
     "answers" : ["6ft", "3.5ft", "5.0ft"],
     "correctAnswer" : "5.0ft"
 }, {
-    "id" : 1,
+    "id" : 2,
     "question" : "What is my favorite food?",
     "answers" : ["Tuna", "Peanut Butter & Honey Sandwich", "Chips"],
     "correctAnswer" : "Chips"
 }, {
-    "id" : 1,
+    "id" : 3,
     "question" : "What is my dog's name?",
     "answers" : ["Beatrix", "Harlequin", "Valentine"],
     "correctAnswer" : "5.0ft"
 }, {
-    "id" : 1,
+    "id" : 5,
     "question" : "If you can't fight 'em...",
     "answers" : ["leave 'em be", "get out of the kitchen", "join 'em"],
     "correctAnswer" : "join 'em"
 }, {
-    "id" : 1,
+    "id" : 6,
     "question" : "What is the meaning of life?",
     "answers" : ["24", "42", "424"],
     "correctAnswer" : "42"
 }]
+
+module.exports = {
+    dummyData;
+}
