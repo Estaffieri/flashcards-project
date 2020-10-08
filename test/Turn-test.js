708748623affb076aeed037b4fee0d955f700d09
Estelle Staffieri <estellelareinne@gmail.com>
2020-10-08 14:20:11 -0600

    Add required files for Turn-test

---
+++ test/Turn-test.js
@@ -0,0 +1,7 @@
+const chai = require("chai");
+const expect = chai.expect;
+
+const dummyData = require("./dummy-data");
+const Card = require("../src/Card");
+const Turn = require("../src/Turn");
+
