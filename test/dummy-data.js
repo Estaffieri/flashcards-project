0cba860bb44778561692da2a9036630804736ec3
Estelle Staffieri <estellelareinne@gmail.com>
2020-10-08 13:26:37 -0600

    Add dummy data for testing

---
+++ test/dummy-data.js
@@ -0,0 +1,26 @@
+const dummyData = [{
+    "id" : 1,
+    "question" : "How tall is Estelle?",
+    "answers" : ["6ft", "3.5ft", "5.0ft"],
+    "correctAnswer" : "5.0ft"
+}, {
+    "id" : 1,
+    "question" : "What is my favorite food?",
+    "answers" : ["Tuna", "Peanut Butter & Honey Sandwich", "Chips"],
+    "correctAnswer" : "Chips"
+}, {
+    "id" : 1,
+    "question" : "What is my dog's name?",
+    "answers" : ["Beatrix", "Harlequin", "Valentine"],
+    "correctAnswer" : "5.0ft"
+}, {
+    "id" : 1,
+    "question" : "If you can't fight 'em...",
+    "answers" : ["leave 'em be", "get out of the kitchen", "join 'em"],
+    "correctAnswer" : "join 'em"
+}, {
+    "id" : 1,
+    "question" : "What is the meaning of life?",
+    "answers" : ["24", "42", "424"],
+    "correctAnswer" : "42"
+}]
