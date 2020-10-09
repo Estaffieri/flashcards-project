3489d61a654653e121676844a8a843b215a8f40d
Estelle Staffieri <estellelareinne@gmail.com>
2020-10-08 18:44:11 -0600

    Add endRound method and clean up testing file

--- src/Round.js
+++ src/Round.js
@@ -1,7 +1,7 @@
 const Turn = require("../src/Turn");
 
 class Round {
-  constructor(deck){
+  constructor(deck) {
     this.deck = deck;
     this.turns = 0;
     this.incorrectGuesses = [];
@@ -13,16 +13,22 @@ class Round {
   takeTurn(guess) {
     const currentCard = this.returnCurrentCard();
     this.currentTurn = new Turn(guess, currentCard);
-    if (!this.currentTurn.evaluateGuess()){
+    if (!this.currentTurn.evaluateGuess()) {
       this.incorrectGuesses.push(currentCard.id);
     }
     this.turns++;
     return this.currentTurn.giveFeedback();
   }
   calculatePercentCorrect() {
-      const amountIncorrect = this.incorrectGuesses.length;
-      const amountCorrect = this.deck.cards.length - amountIncorrect;
-      return Math.floor((amountCorrect / this.deck.cards.length) * 100);
+    const amountIncorrect = this.incorrectGuesses.length;
+    const amountCorrect = this.deck.cards.length - amountIncorrect;
+    return Math.floor((amountCorrect / this.deck.cards.length) * 100);
+  }
+  endRound() {
+    const percentCorrect = this.calculatePercentCorrect();
+    let endRoundMessage = `**Round Over!** You've answered ${percentCorrect}% of the questions correctly!`
+    console.log(endRoundMessage);
+    return endRoundMessage
   }
 }
 
