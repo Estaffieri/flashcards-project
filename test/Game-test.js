5403c4852e078073e4ad5ca61bac3d7ddee62fb1
Estelle Staffieri <estellelareinne@gmail.com>
2020-10-08 18:59:53 -0600

    Test that Game is a function

--- test/Game-test.js
+++ test/Game-test.js
@@ -2,3 +2,11 @@ const chai = require("chai");
 const expect = chai.expect;
 
 const Game = require("../src/Game");
+
+describe.only("Game", function() {
+
+it("should be a function", function() {
+    expect(Game).to.be.a("function");
+    });
+    
+});
