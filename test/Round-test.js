5616ba24595330f5ed4af8dd1044d4bbaa4d4f45
Estelle Staffieri <estellelareinne@gmail.com>
2020-10-08 18:25:53 -0600

    Test that the turns count up by one:

--- test/Round-test.js
+++ test/Round-test.js
@@ -7,19 +7,19 @@ const Deck = require("../src/Deck");
 const Round = require("../src/Round");
 
 describe.only("Round", function() {
-    let card, card1, card2, card3, guess1, guess2, turn1, turn2, deck, deck1, round;
+  let card, card1, card2, card3, guess1, guess2, turn1, turn2, deck, deck1, round;
   beforeEach(function() {
-      card = new Card( 1, "How tall is Estelle?", ["6ft", "3.5ft", "5.0ft"], "5.0ft");
-      guess1 = "5.0ft";
-      guess2 = "left";
-      turn1 = new Turn(guess1, card);
-      turn2 = new Turn(guess2, card);
-      card1 = new Card(1, "What is Sam\'s favorite animal", ["sea otter", "pug", "capybara"], "sea otter");
-      card2 = new Card(2, "What is my favorite food?", ["Tuna", "Peanut Butter & Honey Sandwich", "Chips"], "Chips");
-      card3 = new Card(3, "What is my dog's name?", ["Beatrix", "Harlequin", "Valentine"], "Beatrix");
-      deck = new Deck ();
-      deck1 = new Deck([card, card2, card3]);
-      round = new Round(deck);
+    card = new Card( 1, "How tall is Estelle?", ["6ft", "3.5ft", "5.0ft"], "5.0ft");
+    guess1 = "5.0ft";
+    guess2 = "left";
+    turn1 = new Turn(guess1, card);
+    turn2 = new Turn(guess2, card);
+    card1 = new Card(1, "What is Sam\'s favorite animal", ["sea otter", "pug", "capybara"], "sea otter");
+    card2 = new Card(2, "What is my favorite food?", ["Tuna", "Peanut Butter & Honey Sandwich", "Chips"], "Chips");
+    card3 = new Card(3, "What is my dog's name?", ["Beatrix", "Harlequin", "Valentine"], "Beatrix");
+    deck = new Deck ();
+    deck1 = new Deck([card, card2, card3]);
+    round = new Round(deck);
 
   });
 
@@ -38,6 +38,14 @@ describe.only("Round", function() {
     round.takeTurn("guess");
 
     expect(round.currentTurn).to.be.an.instanceof(Turn);
+  })
+
+  it("should have a way to keep track of the turn", function() {
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+
+    round.takeTurn("guess");
+    expect(round.turns).to.equal(1);
   })
 
 });
