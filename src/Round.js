65e7da5dabbe03958da9f63300e6cc0d28aa79a7
Estelle Staffieri <estellelareinne@gmail.com>
2020-10-08 18:22:59 -0600

    Add takeTurn method to Round class

--- src/Round.js
+++ src/Round.js
@@ -1,9 +1,25 @@
 const Turn = require("../src/Turn");
 
 class Round {
-    constructor() {
-        
+    constructor(deck){
+      this.deck = deck;
+      this.turns = 0;
+      this.incorrectGuesses = [];
+      this.currentTurn = {};
+    }
+    returnCurrentCard() {
+      return this.deck.cards[this.turns];
+    }
+    takeTurn(guess) {
+      const currentCard = this.returnCurrentCard();
+      this.currentTurn = new Turn(guess, currentCard);
+      if (!this.currentTurn.evaluateGuess()){
+        this.incorrectGuesses.push(currentCard.id);
+      }
+      this.turns++;
+      return this.currentTurn.giveFeedback();
     }
 }
+
 
 module.exports = Round;
--- test/Round-test.js
+++ test/Round-test.js
@@ -7,7 +7,7 @@ const Deck = require("../src/Deck");
 const Round = require("../src/Round");
 
 describe.only("Round", function() {
-    let card, card1, card2, card3, guess1, guess2, turn1, turn2, deck1;
+    let card, card1, card2, card3, guess1, guess2, turn1, turn2, deck, deck1, round;
   beforeEach(function() {
       card = new Card( 1, "How tall is Estelle?", ["6ft", "3.5ft", "5.0ft"], "5.0ft");
       guess1 = "5.0ft";
@@ -17,13 +17,28 @@ describe.only("Round", function() {
       card1 = new Card(1, "What is Sam\'s favorite animal", ["sea otter", "pug", "capybara"], "sea otter");
       card2 = new Card(2, "What is my favorite food?", ["Tuna", "Peanut Butter & Honey Sandwich", "Chips"], "Chips");
       card3 = new Card(3, "What is my dog's name?", ["Beatrix", "Harlequin", "Valentine"], "Beatrix");
+      deck = new Deck ();
       deck1 = new Deck([card, card2, card3]);
+      round = new Round(deck);
 
   });
 
   it("should be a function", function() {
     expect(Round).to.be.a("function");
   });
+
+  it("should be an instance of Round", function() {
+    expect(round).to.be.an.instanceof(Round);
+  });
+
+  it("should be an instance of Turn", function() {
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+
+    round.takeTurn("guess");
+
+    expect(round.currentTurn).to.be.an.instanceof(Turn);
+  })
 
 });
   
